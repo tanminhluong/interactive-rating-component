e4aff280a0fd346f1d20231fc13c086a6f32889e
Tấn Minh Lương <tanminhluong99@gmail.com>
2022-11-03 10:33:07 +0700

    finish thank you state

--- asset/main.js
+++ asset/main.js
@@ -9,9 +9,17 @@ const selectElement = (selector) => {
 };
 
 const listContainer = selectElement(".rate-list");
+const submitBtn = selectElement(".btn");
+const normalState = selectElement(".normal");
+const thankyouState = selectElement(".thank-you");
+const thankyouText = selectElement(".thank-you-text");
 
 const listItems = listContainer.getElementsByClassName("list-item");
 
+const getRating = (item) => {
+  return item.getAttribute("data-rate");
+};
+let ratingRes;
 for (var i = 0; i < listItems.length; i++) {
   listItems[i].addEventListener("click", function () {
     let current = document.getElementsByClassName("active");
@@ -23,5 +31,12 @@ for (var i = 0; i < listItems.length; i++) {
 
     // Add the active class to the current/clicked button
     this.className += " active";
+    ratingRes = getRating(this);
   });
 }
+
+submitBtn.addEventListener("click", () => {
+  normalState.style.display = "none";
+  thankyouState.style.display = "block";
+  thankyouText.innerHTML = `You select ${ratingRes} out of 5`;
+});
